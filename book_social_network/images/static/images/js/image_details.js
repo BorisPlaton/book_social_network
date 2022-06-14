0ae83397bbbdf63e390845e2231694c9abca3567
Kirill <pnchkirill@gmail.com>
2022-06-14 06:34:24 +0300

    Templates refactoring

---
+++ book_social_network/images/static/images/js/image_details.js
@@ -0,0 +1,5 @@
+const thumbUpButton = document.querySelector("span a.link-success");
+const thumbDownButton = document.querySelector("span a.link-danger");
+const likesAmount = document.querySelector
+thumbUpButton.addEventListener("click", () => {});
+thumbDownButton.addEventListener("click", () => {});
